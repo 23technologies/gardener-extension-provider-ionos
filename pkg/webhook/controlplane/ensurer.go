1150230669b267c69dc8976e754b725b8617d1b7
Tobias Wolf <wolf@23technologies.cloud>
2021-12-02 14:51:38 +0100

    Temporarily work around issues with a valid hostname for pods

--- pkg/webhook/controlplane/ensurer.go
+++ pkg/webhook/controlplane/ensurer.go
@@ -121,7 +121,7 @@ func (e *ensurer) EnsureKubeletServiceUnitOptions(ctx context.Context, gctx gcon
 	new = extensionswebhook.EnsureUnitOption(new, &unit.UnitOption{
 		Section: "Service",
 		Name:    "ExecStartPre",
-		Value:   `/bin/sh -c 'hostnamectl set-hostname $(cat /etc/hostname | cut -d '.' -f 1)'`,
+		Value:   `/bin/sh -c 'hostnamectl set-hostname $(cat /etc/machine-id)'`,
 	})
 	return new, nil
 }
