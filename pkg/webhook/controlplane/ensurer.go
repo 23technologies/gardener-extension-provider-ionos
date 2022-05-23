d10cd74b3b2bf82cf19b74f1f2ea942c87ff74af
Tobias Wolf <wolf@23technologies.cloud>
2022-05-23 21:08:34 +0200

    Temporarily work around issues with a valid hostname for pods in MCM
    This reverts commit 800ea461fbb994192c9b1bc712634f71642f8955.

--- pkg/webhook/controlplane/ensurer.go
+++ pkg/webhook/controlplane/ensurer.go
@@ -115,7 +115,7 @@ func (e *ensurer) EnsureKubeletServiceUnitOptions(ctx context.Context, gctx gcon
 	new = extensionswebhook.EnsureUnitOption(new, &unit.UnitOption{
 		Section: "Service",
 		Name:    "ExecStartPre",
-		Value:   `/bin/sh -c 'hostnamectl set-hostname $(uuidgen)'`,
+		Value:   `/bin/sh -c 'hostnamectl set-hostname $(cat /etc/hostname | cut -d '.' -f 1)'`,
 	})
 	return new, nil
 }
